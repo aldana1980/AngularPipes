ce4f90276b30da4d0b4fd14bb8a35c8095d7034d
aldana1980 <programaionjava@gmail.com>
2020-06-27 21:46:27 -0300

    Proyecto Pipes en Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+nombre   :string   = "Aldana";
+nombre2   :string   =  'RAlphEs GoMeZ';
+
+arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+PI            : number = Math.PI;
+porcentaje:number = 0.234;
+salario :number = 1234.5;
+fecha   :Date = new Date();
+activar : boolean =true;
+
+idioma  :string = 'fr';
+videoUrl :string = 'https://www.youtube.com/embed/onDwil6yrAA';
+
+
+valorPromesa = new Promise<string>( (resolve)  => {
+
+
+  setTimeout(() => {
+    resolve('llego la data');
+  },3500);
+});
+
+
+
+
+heroe = {
+  nombre: 'Logan',
+  clave: 'Wolverine',
+  edad: 500,
+  direccion: {
+    calle: 'Primera',
+    casa: 20
+  }
+
+}
+
+}
